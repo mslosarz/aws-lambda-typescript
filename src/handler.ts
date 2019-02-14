f5f6c3f8a1089805885aa67466bf98f32b080dd1
Marcin Ślósarz <marcin.slosarz@software2.pl>
2019-02-14 23:25:53 +0100

    npm run build will produce working lambda

--- src/handler.ts
+++ src/handler.ts
@@ -1,9 +1,10 @@
 import { APIGatewayProxyEvent, APIGatewayProxyResult } from 'aws-lambda'
+import moment from 'moment'
 
 export const handler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
   return {
     statusCode: 200,
-    body: 'Connected.'
+    body: moment.utc().toISOString()
   }
 }
 
--- test/handler.spec.ts
+++ test/handler.spec.ts
@@ -1,52 +1,62 @@
-import {handler} from '../src/handler'
-import {expect} from 'chai'
+import { handler } from '../src/handler'
+import { expect } from 'chai'
+import * as sinon from 'sinon'
 import 'mocha'
+import moment from 'moment'
 
 describe('handler', () => {
-    const dummyEvent = {
-        headers: {},
-        body: null,
-        multiValueHeaders: {},
-        httpMethod: 'GET',
-        isBase64Encoded: false,
-        path: 'xx',
-        pathParameters: null,
-        queryStringParameters: null,
-        multiValueQueryStringParameters: null,
-        stageVariables: null,
-        requestContext: {
-            accountId: '',
-            apiId: '',
-            connectedAt: 100,
-            httpMethod: 'GET',
-            identity: {
-                accessKey: null,
-                accountId: null,
-                apiKey: null,
-                apiKeyId: null,
-                caller: null,
-                cognitoAuthenticationProvider: null,
-                cognitoAuthenticationType: null,
-                cognitoIdentityId: null,
-                cognitoIdentityPoolId: null,
-                sourceIp: '10.10.10.10',
-                user: null,
-                userAgent: null,
-                userArn: null
-            },
-            path: 'xx',
-            stage: 'test',
-            requestId: 'id',
-            requestTimeEpoch: 123,
-            resourceId: 'a',
-            resourcePath: 'xxx'
-        },
-        resource: ''
-    };
+  const dummyEvent = {
+    headers: {},
+    body: null,
+    multiValueHeaders: {},
+    httpMethod: 'GET',
+    isBase64Encoded: false,
+    path: 'xx',
+    pathParameters: null,
+    queryStringParameters: null,
+    multiValueQueryStringParameters: null,
+    stageVariables: null,
+    requestContext: {
+      accountId: '',
+      apiId: '',
+      connectedAt: 100,
+      httpMethod: 'GET',
+      identity: {
+        accessKey: null,
+        accountId: null,
+        apiKey: null,
+        apiKeyId: null,
+        caller: null,
+        cognitoAuthenticationProvider: null,
+        cognitoAuthenticationType: null,
+        cognitoIdentityId: null,
+        cognitoIdentityPoolId: null,
+        sourceIp: '10.10.10.10',
+        user: null,
+        userAgent: null,
+        userArn: null
+      },
+      path: 'xx',
+      stage: 'test',
+      requestId: 'id',
+      requestTimeEpoch: 123,
+      resourceId: 'a',
+      resourcePath: 'xxx'
+    },
+    resource: ''
+  }
 
-    it('should return 200 always!', async () => {
-        const result = await handler(dummyEvent)
-        expect(result).to.be.deep.equal({statusCode: 200, body: 'Connected.'})
-    })
+  before(function () {
+    this.clock = sinon.useFakeTimers(new Date(2012, 1, 10).getTime())
+  })
+
+  after(function () {
+    this.clock.restore()
+  })
+
+  it('should return 200 always!', async () => {
+    const result = await handler(dummyEvent)
+    expect(result).to.be.deep.equal({statusCode: 200, body: moment().utc().toISOString()})
+  })
 
 })
